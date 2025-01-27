54fc37889ded48b8b6f42398f49c0cd9abad6a62
frandd07 <150445758+frandd07@users.noreply.github.com>
2025-01-27 17:24:04 +0100

    Update

--- src/app/api/libro/route.js
+++ src/app/api/libro/route.js
@@ -1,46 +1,63 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://eyaraedkwjgspknimmfb.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5YXJhZWRrd2pnc3BrbmltbWZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY3NjI4MTgsImV4cCI6MjA1MjMzODgxOH0.6GEOM6YASqLMdFYTmNW7ceebdOKKxtVMQjUqlm8VWWg"
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = "https://eyaraedkwjgspknimmfb.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5YXJhZWRrd2pnc3BrbmltbWZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY3NjI4MTgsImV4cCI6MjA1MjMzODgxOH0.6GEOM6YASqLMdFYTmNW7ceebdOKKxtVMQjUqlm8VWWg";
+const supabase = createClient(supabaseUrl, supabaseKey);
 
-export async function GET(){
-    const {data: libros, error} = await supabase
-    .from('libro')
-    .select('id,titulo,autor,leido')
+export async function GET() {
+  const { data: libros, error } = await supabase
+    .from("libro")
+    .select("id,titulo,autor,leido");
 
-    return new Response(JSON.stringify(libros), {status:200})
+  return new Response(JSON.stringify(libros), { status: 200 });
 }
 
-export async function PUT(request){
-    const body = await request.json()
-    const id = body.id
-    const{data: updateData, error} = await supabase
+export async function PUT(request) {
+  const body = await request.json();
+  const id = body.id;
+  const { data: updateData, error } = await supabase
     .from("libro")
     .update(body.update)
-    .eq("id",id)
+    .eq("id", id);
 
-    return new Response(JSON.stringify({success: "Actualizado"}, {status:200}))
+  return new Response(
+    JSON.stringify({ success: "Actualizado" }, { status: 200 })
+  );
 }
 
-export async function DELETE(request){
-    const body = await request.json()
-    const id = body.id
+export async function DELETE(request) {
+  const body = await request.json();
+  const id = body.id;
 
-    const {data: deleteData, error} = await supabase
+  const { data: deleteData, error } = await supabase
     .from("libro")
     .delete()
-    .eq("id", id)
-    
-    return new Response(JSON.stringify({success: "Eliminado correctamente"}), {status:200})
+    .eq("id", id);
+
+  return new Response(JSON.stringify({ success: "Eliminado correctamente" }), {
+    status: 200,
+  });
 }
 
-export async function POST(request){
-    const body = await request.json()
-    const libro = body.libro
-    const {data: postData, error} = await supabase
-    .from("libro")
-    .insert(libro)
+export async function POST(request) {
+  const body = await request.json();
+  const libro = body.libro;
 
-    return new Response(JSON.stringify({success: "Creado correctamente"}), {status:200})
+  if (titulo !== "" && autor !== "") {
+    const { data: postData, error } = await supabase
+      .from("libro")
+      .insert(libro);
+
+    if (!error) {
+      return new Response(JSON.stringify({ success: "Creado correctamente" }), {
+        status: 200,
+      });
+    }
+  } else {
+    return new Response(
+      JSON.stringify({ error: "Alguno de los campos esta vacío" }),
+      { status: 400 }
+    );
+  }
 }
--- src/app/libro/create/page.js
+++ src/app/libro/create/page.js
@@ -1,71 +1,74 @@
-'use client'
+"use client";
 
-import { use, useState } from "react"
+import { use, useState } from "react";
 
-export default function CreateLibro(){
-    const[titulo,setTitulo]  = useState("")
-    const[autor,setAutor]  = useState("")
-    const[leido, setLeido] = useState(false)
-    
+export default function CreateLibro() {
+  const [titulo, setTitulo] = useState("");
+  const [autor, setAutor] = useState("");
+  const [leido, setLeido] = useState(false);
 
-    
+  async function crearLibro() {
+    if (titulo !== "" && autor !== "") {
+      const response = await fetch("/api/libro", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          libro: {
+            titulo: titulo,
+            autor: autor,
+            leido: leido,
+          },
+        }),
+      });
 
-    async function crearLibro(){
-        const response = await fetch("/api/libro", {
-            method: 'POST',
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({
-                libro: {
-                    titulo: titulo,
-                    autor: autor,
-                    leido: leido
-                }
-            })
-        })
-
-        if(response.ok){
-            alert("Libro creado")
-            setTitulo("")
-            setAutor("")
-            setLeido(false)
-        }else{
-            alert("Hubo un error al crearlo")
-        }
+      if (response.ok) {
+        alert("Libro creado");
+        setTitulo("");
+        setAutor("");
+        setLeido(false);
+      } else {
+        alert("Hubo un error al crearlo");
+      }
+    } else {
+      alert("Titulo o autor vacíos");
     }
+  }
 
-    return(
-        <div>
-            <h1>Crear nuevo libro</h1>
-            <form onSubmit={crearLibro}>
-                <label>
-                    Título: 
-                <input 
-                type="text"
-                value={titulo}
-                onChange={(e) => setTitulo(e.target.value)}
-                />
-                </label>
-                <br/>
-                <label>
-                    Autor
-                <input 
-                type="text"
-                value={autor}
-                onChange={(e) => setAutor(e.target.value)}
-                />
-                </label>
-                <br/>
-                <label>
-                    ¿Lo has leido? 
-                <input 
-                type="checkbox"
-                value={leido}
-                onChange={(e) => setLeido(e.target.checked)}
-                />
-                <br/>
-                <input type="submit" value="Añadir libro"/>
-                </label>
-            </form>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Crear nuevo libro</h1>
+      <form onSubmit={crearLibro}>
+        <label>
+          Título:
+          <input
+            type="text"
+            value={titulo}
+            onChange={(e) => setTitulo(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Autor
+          <input
+            type="text"
+            value={autor}
+            onChange={(e) => setAutor(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          ¿Lo has leido?
+          <input
+            type="checkbox"
+            value={leido}
+            onChange={(e) => setLeido(e.target.checked)}
+          />
+          <br />
+          <input type="submit" value="Añadir libro" />
+        </label>
+      </form>
+    </div>
+  );
 }
